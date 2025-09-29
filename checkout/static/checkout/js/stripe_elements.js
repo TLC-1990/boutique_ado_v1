7a6ee415d9e6169380e2c05ac86863397d122b54
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-09-29 22:04:49 +0100

    add loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -55,13 +55,14 @@ form.addEventListener('submit', function (ev) {
         'disabled': true
     });
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
         }
     }).then(function (result) {
         if (result.error) {
-            // show error to your customer (e.g., insufficient funds)
             var errorDiv = document.getElementById('card-errors');
             var html = `
                 <span class="icon" role="alert">
@@ -69,19 +70,15 @@ form.addEventListener('submit', function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({
                 'disabled': false
             });
             $('#submit-button').attr('disabled', false);
         } else {
-            // The payment has been processed!
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
-                // show a success message to your customer
-                // There's a risk of the customer closing the window before callback
-                // execution. Set up a webhook or plugin to listen for the
-                // payment_intent.succeeded event that handles any business critical
-                // post-payment actions.
             }
         }
     });
