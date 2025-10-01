2faa7b1e7d30a269debaa5567c0c7cb35920195a
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-01 22:18:48 +0100

    complete webhook handler

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,12 +1,11 @@
-from django.http import HttpResponse
 from django.conf import settings
+from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
 from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
-
 
 @require_POST
 @csrf_exempt
@@ -16,15 +15,16 @@ def webhook(request):
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # get the webhook data and verify its signature
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_WH_SECRET
+        payload, sig_header, wh_secret
         )
+
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -51,7 +51,8 @@ def webhook(request):
     # If there's a handler for it, get it from the event map
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
-    
+
     # Call the event handler with the event
     response = event_handler(event)
+    
     return response
